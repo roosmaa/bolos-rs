16aa1681a53a34325ca3e8c9489c99773b78bf4b
Mart Roosmaa <mart@roosmaa.net>
2018-05-31 12:30:51 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,91 @@
+#*******************************************************************************
+#   Ledger App
+#   (c) 2017 Ledger
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#*******************************************************************************
+
+ifeq ($(BOLOS_SDK),)
+$(error Environment variable BOLOS_SDK is not set)
+endif
+include $(BOLOS_SDK)/Makefile.defines
+
+APPNAME ="Rusty"
+APP_LOAD_PARAMS=--appFlags 0x50 --path "123'/435'" --curve ed25519 $(COMMON_LOAD_PARAMS)
+
+APPVERSION_M=0
+APPVERSION_N=1
+APPVERSION_P=0
+APPVERSION=$(APPVERSION_M).$(APPVERSION_N).$(APPVERSION_P)
+
+ICONNAME=nanos_icon.gif
+
+ifeq (customCA.key,$(wildcard customCA.key))
+	CUSTOM_CA_PARAM=--rootPrivateKey `cat customCA.key`
+else
+	CUSTOM_CA_PARAM=
+endif
+
+################
+# Default rule #
+################
+
+all: default
+
+############
+# Platform #
+############
+
+DEFINES   += OS_IO_SEPROXYHAL IO_SEPROXYHAL_BUFFER_SIZE_B=300
+DEFINES   += HAVE_BAGL HAVE_SPRINTF
+#DEFINES   += HAVE_PRINTF PRINTF=screen_printf
+DEFINES   += PRINTF\(...\)=
+DEFINES   += HAVE_IO_USB HAVE_L4_USBLIB IO_USB_MAX_ENDPOINTS=6 IO_HID_EP_LENGTH=64 HAVE_USB_APDU
+DEFINES   += APP_MAJOR_VERSION=$(APPVERSION_M) APP_MINOR_VERSION=$(APPVERSION_N) APP_PATCH_VERSION=$(APPVERSION_P)
+
+##############
+# Compiler #
+##############
+#GCCPATH   := $(BOLOS_ENV)/gcc-arm-none-eabi-5_3-2016q1/bin/
+#CLANGPATH := $(BOLOS_ENV)/clang-arm-fropi/bin/
+CC       := $(CLANGPATH)clang
+
+#CFLAGS   += -O0
+CFLAGS   += -O3 -Os -Wno-typedef-redefinition
+
+AS     := $(GCCPATH)arm-none-eabi-gcc
+
+LD       := $(GCCPATH)arm-none-eabi-gcc
+LDFLAGS  += -O3 -Os
+LDLIBS   += -lm -lgcc -lc
+
+# import rules to compile glyphs(/pone)
+include $(BOLOS_SDK)/Makefile.glyphs
+
+### variables processed by the common makefile.rules of the SDK to grab source files and include dirs
+APP_SOURCE_PATH  += src
+SDK_SOURCE_PATH  += lib_stusb
+SDK_SOURCE_PATH  += lib_stusb_impl
+
+load: all
+	python -m ledgerblue.loadApp $(CUSTOM_CA_PARAM) $(APP_LOAD_PARAMS)
+
+delete:
+	python -m ledgerblue.deleteApp $(CUSTOM_CA_PARAM) $(COMMON_DELETE_PARAMS)
+
+# import generic rules from the sdk
+include $(BOLOS_SDK)/Makefile.rules
+
+#add dependency on custom makefile filename
+dep/%.d: %.c Makefile
+
---
+++ src/main.c
@@ -0,0 +1,157 @@
+/*******************************************************************************
+*   $NANO Wallet for Ledger Nano S & Blue
+*   (c) 2018 Mart Roosmaa
+*   (c) 2016 Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+
+#include <string.h>
+#include <stdbool.h>
+
+#include "os.h"
+#include "os_io_seproxyhal.h"
+#include "cx.h"
+
+void ui_idle(void);
+void ui_ticker_event(bool uxAllowed);
+
+uint8_t G_io_seproxyhal_spi_buffer[IO_SEPROXYHAL_BUFFER_SIZE_B];
+
+// override point, but nothing more to do
+void io_seproxyhal_display(const bagl_element_t *element) {
+    io_seproxyhal_display_default((bagl_element_t *)element);
+}
+
+uint16_t io_exchange_al(uint8_t channel, uint16_t tx_len) {
+    switch (channel & ~(IO_FLAGS)) {
+    case CHANNEL_KEYBOARD:
+        break;
+
+    // multiplexed io exchange over a SPI channel and TLV encapsulated protocol
+    case CHANNEL_SPI:
+        if (tx_len) {
+            io_seproxyhal_spi_send(G_io_apdu_buffer, tx_len);
+
+            if (channel & IO_RESET_AFTER_REPLIED) {
+                reset();
+            }
+            return 0; // nothing received from the master so far (it's a tx
+                      // transaction)
+        } else {
+            return io_seproxyhal_spi_recv(G_io_apdu_buffer,
+                                          sizeof(G_io_apdu_buffer), 0);
+        }
+
+    default:
+        THROW(INVALID_PARAMETER);
+    }
+    return 0;
+}
+
+uint8_t io_event(uint8_t channel) {
+    // nothing done with the event, throw an error on the transport layer if
+    // needed
+
+    // can't have more than one tag in the reply, not supported yet.
+    switch (G_io_seproxyhal_spi_buffer[0]) {
+    case SEPROXYHAL_TAG_FINGER_EVENT:
+        UX_FINGER_EVENT(G_io_seproxyhal_spi_buffer);
+        break;
+
+    case SEPROXYHAL_TAG_BUTTON_PUSH_EVENT:
+        UX_BUTTON_PUSH_EVENT(G_io_seproxyhal_spi_buffer);
+        break;
+
+    case SEPROXYHAL_TAG_STATUS_EVENT:
+        if (G_io_apdu_media == IO_APDU_MEDIA_USB_HID &&
+            !(U4BE(G_io_seproxyhal_spi_buffer, 3) &
+              SEPROXYHAL_TAG_STATUS_EVENT_FLAG_USB_POWERED)) {
+            THROW(EXCEPTION_IO_RESET);
+        }
+    // no break is intentional
+    default:
+        UX_DEFAULT_EVENT();
+        break;
+
+    case SEPROXYHAL_TAG_DISPLAY_PROCESSED_EVENT:
+        UX_DISPLAYED_EVENT({
+        });
+        break;
+
+    case SEPROXYHAL_TAG_TICKER_EVENT:
+        UX_TICKER_EVENT(G_io_seproxyhal_spi_buffer, {
+            ui_ticker_event(UX_ALLOWED);
+        });
+        break;
+    }
+
+    // close the event if not done previously (by a display or whatever)
+    if (!io_seproxyhal_spi_is_status_sent()) {
+        io_seproxyhal_general_status();
+    }
+
+    // command has been processed, DO NOT reset the current APDU transport
+    return 1;
+}
+
+void app_exit(void) {
+    BEGIN_TRY_L(exit) {
+        TRY_L(exit) {
+            os_sched_exit(-1);
+        }
+        FINALLY_L(exit) {
+        }
+    }
+    END_TRY_L(exit);
+}
+
+__attribute__((section(".boot"))) int main(void) {
+    // exit critical section
+    __asm volatile("cpsie i");
+
+    // ensure exception will work as planned
+    os_boot();
+
+    for (;;) {
+        UX_INIT();
+        BEGIN_TRY {
+            TRY {
+                io_seproxyhal_init();
+
+                // deactivate usb before activating
+                USB_power(false);
+                USB_power(true);
+
+                ui_idle();
+
+                for (;;) {
+                    io_exchange(CHANNEL_APDU, 0);
+                }
+            }
+            CATCH(EXCEPTION_IO_RESET) {
+                // reset IO and UX
+                continue;
+            }
+            CATCH_ALL {
+                break;
+            }
+            FINALLY {
+            }
+        }
+        END_TRY;
+    }
+    app_exit();
+
+    return 0;
+}
