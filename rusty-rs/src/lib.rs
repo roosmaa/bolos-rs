9acfac266ef36665a70f6d4af33bf0bda32d723d
Mart Roosmaa <mart@roosmaa.net>
2018-06-03 01:34:53 +0200

    Add boilerplate for UI views

--- rusty-rs/src/lib.rs
+++ rusty-rs/src/lib.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![feature(asm)]
+#![allow(dead_code)]
 
 extern crate panic_abort;
 
 mod error;
 mod syscall;
+mod ui;
 
 use core::slice;
 
@@ -25,4 +27,54 @@ pub extern "C" fn update_label(ptr: *mut u8, len: usize) {
     buf[2] = 'a' as u8 + (buf[2] % ('z' as u8 - 'a' as u8));
     buf[3] = 'a' as u8 + (buf[3] % ('z' as u8 - 'a' as u8));
     buf[4] = 0;
+}
+
+static mut UI: [ui::View; 10] = [ui::View::None; 10];
+
+fn ui_idle() {
+    let next_ui: [ui::View; 5] = [
+        ui::RectangleView{
+            frame: ui::Frame{ x: 0, y: 0, width: 128, height: 32 },
+            fill: ui::FillMode::Fill,
+            ..Default::default()
+        }.into(),
+        ui::IconView{
+            frame: ui::Frame{ x: 3, y: 12, width: 7, height: 7 },
+            icon: ui::Icon::Cross,
+            ..Default::default()
+        }.into(),
+        ui::IconView{
+            frame: ui::Frame{ x: 117, y: 13, width: 8, height: 6 },
+            icon: ui::Icon::Check,
+            ..Default::default()
+        }.into(),
+        ui::LabelLineView{
+            frame: ui::Frame{ x: 0, y: 12, width: 128, height: 12 },
+            font: ui::TextFont::OpenSansRegular11px,
+            horizontal_alignment: ui::TextHorizontalAlignment::Center,
+            text: "Hello!".as_bytes(),
+            ..Default::default()
+        }.into(),
+        ui::LabelLineView{
+            user_id: 0x02,
+            frame: ui::Frame{ x: 23, y: 26, width: 82, height: 12 },
+            font: ui::TextFont::OpenSansRegular11px,
+            horizontal_alignment: ui::TextHorizontalAlignment::Center,
+            scroll: ui::ScrollMode::Once{ delay: 10, speed: 26 },
+            text: "Rust".as_bytes(),
+            ..Default::default()
+        }.into(),
+    ];
+
+    let ui: &'static mut [ui::View] = unsafe { &mut UI };
+    let len = if next_ui.len() >= ui.len() {
+        ui.len()
+    } else {
+        next_ui.len()
+    };
+
+    ui[..len].copy_from_slice(&next_ui[..len]);
+    if len < ui.len() {
+        ui[len] = ui::View::None;
+    }
 }
---
+++ rusty-rs/src/ui.rs
@@ -0,0 +1,173 @@
+use core::convert::Into;
+
+#[derive(Copy, Clone)]
+pub enum FillMode {
+    Fill,
+    NoFill,
+}
+
+#[derive(Copy, Clone)]
+pub struct Color(u32);
+
+#[derive(Copy, Clone)]
+pub struct Frame {
+    pub x: i16,
+    pub y: i16,
+    pub width: u16,
+    pub height: u16,
+}
+
+impl Default for Frame {
+    fn default() -> Self {
+        Self{ x: 0, y: 0, width: 0, height: 0 }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct RectangleView {
+    pub user_id: u8,
+    pub frame: Frame,
+    pub stroke: u8,
+    pub radius: u8,
+    pub fill: FillMode,
+    pub foreground: Color,
+    pub background: Color,
+}
+
+impl Default for RectangleView {
+    fn default() -> Self {
+        Self{
+            user_id: 0,
+            frame: Default::default(),
+            stroke: 0,
+            radius: 0,
+            fill: FillMode::NoFill,
+            foreground: Color(0x000000),
+            background: Color(0xFFFFFF),
+        }
+    }
+}
+
+impl Into<View> for RectangleView {
+    fn into(self) -> View {
+        View::Rectangle(self)
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct CustomIcon {
+    pub width: u32,
+    pub height: u32,
+    pub bits_per_pixel: u32,
+    pub colors: &'static [Color],
+    pub bitmap: &'static [u8],
+}
+
+impl Into<Icon> for CustomIcon {
+    fn into(self) -> Icon {
+        Icon::Custom(self)
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Icon {
+    Check,
+    Cross,
+    Custom(CustomIcon),
+}
+
+#[derive(Copy, Clone)]
+pub struct IconView {
+    pub user_id: u8,
+    pub frame: Frame,
+    pub icon: Icon,
+}
+
+impl Default for IconView {
+    fn default() -> Self {
+        Self{
+            user_id: 0,
+            frame: Default::default(),
+            icon: CustomIcon{
+                width: 0,
+                height: 0,
+                bits_per_pixel: 0,
+                colors: &[],
+                bitmap: &[],
+            }.into(),
+        }
+    }
+}
+
+impl Into<View> for IconView {
+    fn into(self) -> View {
+        View::Icon(self)
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum ScrollMode {
+    Disabled,
+    Once{ delay: u8, speed: u8 },
+    Infinite{ delay: u8, speed: u8 }
+}
+
+#[derive(Copy, Clone)]
+pub enum TextHorizontalAlignment {
+    Left,
+    Center,
+    Right,
+}
+
+#[derive(Copy, Clone)]
+pub enum TextVerticalAlignment {
+    Top,
+    Middle,
+    Bottom,
+}
+
+#[derive(Copy, Clone)]
+pub enum TextFont {
+    OpenSansLight16px,
+    OpenSansRegular11px,
+    OpenSansExtraBold11px,
+}
+
+#[derive(Copy, Clone)]
+pub struct LabelLineView {
+    pub user_id: u8,
+    pub frame: Frame,
+    pub font: TextFont,
+    pub horizontal_alignment: TextHorizontalAlignment,
+    pub vertical_alignment: TextVerticalAlignment,
+    pub scroll: ScrollMode,
+    pub text: &'static [u8],
+}
+
+impl Default for LabelLineView {
+    fn default() -> Self {
+        Self{
+            user_id: 0,
+            frame: Default::default(),
+            font: TextFont::OpenSansRegular11px,
+            horizontal_alignment: TextHorizontalAlignment::Left,
+            vertical_alignment: TextVerticalAlignment::Top,
+            scroll: ScrollMode::Disabled,
+            text: &[0],
+        }
+    }
+}
+
+impl Into<View> for LabelLineView {
+    fn into(self) -> View {
+        View::LabelLine(self)
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum View {
+    None,
+    Rectangle(RectangleView),
+    Icon(IconView),
+    LabelLine(LabelLineView),
+}
