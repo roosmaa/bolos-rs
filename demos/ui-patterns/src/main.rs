8ea4c84d7e8a4613fc089e53b80891318c1d2c55
Mart Roosmaa <mart@roosmaa.net>
2018-07-24 10:39:50 +0200

    Add demo confirmation UI

--- demos/ui-patterns/src/main.rs
+++ demos/ui-patterns/src/main.rs
@@ -8,16 +8,71 @@ mod icon;
 
 use bolos::seproxyhal::MessageLoop;
 use bolos::runtime::exit;
+use bolos::time::{Duration};
 use bolos::ui;
 use bolos::ui::menu;
 use bolos::state::{Store, BasicAction};
 
 enum UiState {
     MainMenu(MainMenuItem),
+    Demo(DemoState),
     SettingsMenu(SettingsMenuItem),
     TruncateAddressMenu(TruncateAddressMenuItem),
     RecipientMenu(RecipientMenuItem),
     AboutMenu(AboutMenuItem),
+}
+
+#[derive(Copy, Clone)]
+enum DemoState {
+    Notice,
+    Address,
+    Recipient,
+}
+
+impl DemoState {
+    fn next(&self) -> Self {
+        match *self {
+            DemoState::Notice => DemoState::Address,
+            DemoState::Address => DemoState::Recipient,
+            DemoState::Recipient => DemoState::Notice,
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum MainMenuItem {
+    RunDemo,
+    Settings,
+    Quit,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum SettingsMenuItem {
+    TruncateAddress,
+    Recipient,
+    About,
+    Back,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum TruncateAddressMenuItem {
+    Yes,
+    No,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum RecipientMenuItem {
+    Government,
+    Charity,
+    Myself,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum AboutMenuItem {
+    Version,
+    Developer,
+    SourceCode,
+    Back,
 }
 
 enum Recipient {
@@ -27,6 +82,7 @@ enum Recipient {
 }
 
 struct AppState {
+    demo_confirms: u32,
     truncate_address: bool,
     recipient: Recipient,
     ui_state: UiState,
@@ -36,6 +92,7 @@ struct AppState {
 impl AppState {
     fn new() -> Self {
         Self{
+            demo_confirms: 0,
             truncate_address: false,
             recipient: Recipient::Charity,
             ui_state: UiState::MainMenu(MainMenuItem::RunDemo),
@@ -67,9 +124,23 @@ impl Store for AppState {
                         self.update_ui(UiState::MainMenu(new_item));
                     },
                     BasicAction::Confirm => match current_item {
+                        MainMenuItem::RunDemo => self.update_ui(UiState::Demo(DemoState::Notice)),
                         MainMenuItem::Settings => self.update_ui(UiState::SettingsMenu(SettingsMenuItem::TruncateAddress)),
                         MainMenuItem::Quit => exit(0),
-                        _ => self.update_ui(UiState::MainMenu(MainMenuItem::Quit)),
+                    },
+                }
+            },
+            UiState::Demo(state) => {
+                match action {
+                    BasicAction::Previous => {
+                        self.update_ui(UiState::MainMenu(MainMenuItem::RunDemo));
+                    },
+                    BasicAction::Confirm => {
+                        self.demo_confirms += 1;
+                        self.update_ui(UiState::MainMenu(MainMenuItem::RunDemo));
+                    },
+                    BasicAction::Next => {
+                        self.update_ui(UiState::Demo(state.next()));
                     },
                 }
             },
@@ -184,53 +255,96 @@ impl ui::Delegate for AppState {
             UiState::TruncateAddressMenu(item) => menu::prepare_menu(item, self, ctrl),
             UiState::RecipientMenu(item) => menu::prepare_menu(item, self, ctrl),
             UiState::AboutMenu(item) => menu::prepare_menu(item, self, ctrl),
+
+            UiState::Demo(state) => {
+                ctrl.set_button_actions(ui::ButtonAction::Map{
+                    left: Some(BasicAction::Previous),
+                    right: Some(BasicAction::Confirm),
+                    both: None,
+                });
+
+                ctrl.add_view(|| ui::RectangleView{
+                    frame: ui::Frame{ x: 0, y: 0, width: 128, height: 32 },
+                    fill: ui::FillMode::Fill,
+                    ..Default::default()
+                }.into());
+                ctrl.add_view(|| ui::IconView{
+                    position: ui::Position{ x: 3, y: 12 },
+                    icon: ui::SystemIcon::Cross.into(),
+                    ..Default::default()
+                }.into());
+                ctrl.add_view(|| ui::IconView{
+                    position: ui::Position{ x: 117, y: 13 },
+                    icon: ui::SystemIcon::Check.into(),
+                    ..Default::default()
+                }.into());
+
+                let line_1_base = ui::LabelLineView{
+                    frame: ui::Frame{ x: 0, y: 12, width: 128, height: 12 },
+                    font: ui::TextFont::OpenSansExtraBold11px,
+                    horizontal_alignment: ui::TextHorizontalAlignment::Center,
+                    ..Default::default()
+                };
+                let line_2_base = ui::LabelLineView{
+                    frame: ui::Frame{ x: 23, y: 26, width: 82, height: 12 },
+                    font: ui::TextFont::OpenSansRegular11px,
+                    horizontal_alignment: ui::TextHorizontalAlignment::Center,
+                    scroll: ui::ScrollMode::Once{ delay_secs: 10, speed: 26 },
+                    ..Default::default()
+                };
+
+                match state {
+                    DemoState::Notice => {
+                        ctrl.add_view(|| ui::LabelLineView{
+                            text: "Confirm",
+                            ..line_1_base
+                        }.into());
+                        ctrl.add_view(|| ui::LabelLineView{
+                            font: ui::TextFont::OpenSansExtraBold11px,
+                            text: "details",
+                            scroll: ui::ScrollMode::Disabled,
+                            ..line_2_base
+                        }.into());
+                    },
+                    DemoState::Address => {
+                        ctrl.add_view(|| ui::LabelLineView{
+                            text: "Address",
+                            ..line_1_base
+                        }.into());
+                        ctrl.add_view(|| ui::LabelLineView{
+                            text: match self.truncate_address {
+                                true => "4ecb..4d51",
+                                false => "4ecbde854d829816358041c8b393fa4d51",
+                            },
+                            ..line_2_base
+                        }.into());
+                    },
+                    DemoState::Recipient => {
+                        ctrl.add_view(|| ui::LabelLineView{
+                            text: "Recipient",
+                            ..line_1_base
+                        }.into());
+                        ctrl.add_view(|| ui::LabelLineView{
+                            text: match self.recipient {
+                                Recipient::Government => "Government",
+                                Recipient::Charity => "Charity",
+                                Recipient::Myself => "Myself",
+                            },
+                            ..line_2_base
+                        }.into());
+                    },
+                }
+
+                ctrl.set_auto_action(ui::AutoAction::Countdown{
+                    min_wait_time: Some(Duration::from_millis(3000)),
+                    max_wait_time: Some(Duration::from_secs(10)),
+                    wait_time: Duration::from_millis(1000),
+                    wait_for_scroll: true,
+                    action: BasicAction::Next,
+                });
+            },
         }
-
-        // ctrl.set_auto_action(ui::AutoAction::Countdown{
-        //     min_wait_time: Some(Duration::from_millis(3000)),
-        //     max_wait_time: Some(Duration::from_secs(10)),
-        //     wait_time: Duration::from_millis(1000),
-        //     wait_for_scroll: true,
-        //     action: BasicAction::Next,
-        // });
     }
-}
-
-#[derive(Copy, Clone, PartialEq, Eq)]
-enum MainMenuItem {
-    RunDemo,
-    Settings,
-    Quit,
-}
-
-
-#[derive(Copy, Clone, PartialEq, Eq)]
-enum SettingsMenuItem {
-    TruncateAddress,
-    Recipient,
-    About,
-    Back,
-}
-
-#[derive(Copy, Clone, PartialEq, Eq)]
-enum TruncateAddressMenuItem {
-    Yes,
-    No,
-}
-
-#[derive(Copy, Clone, PartialEq, Eq)]
-enum RecipientMenuItem {
-    Government,
-    Charity,
-    Myself,
-}
-
-#[derive(Copy, Clone, PartialEq, Eq)]
-enum AboutMenuItem {
-    Version,
-    Developer,
-    SourceCode,
-    Back,
 }
 
 impl menu::Delegate<MainMenuItem> for AppState {
